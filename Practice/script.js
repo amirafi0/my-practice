89694b309747da1b70557bfed5b194f12cd16c9b
Ami rafi <kh9961734@gmail.com>
2024-02-18 10:50:57 +0600

    my practice

---
+++ Practice/script.js
@@ -0,0 +1,44 @@
+// 1.find the numbers of characters
+
+// let myName = "Kamrul Hasan Rafi";
+
+// console.log(myName.length)
+
+
+
+//  2.to uppercase
+
+// let myName = "Kamrul Hasan Rafi";
+
+// console.log(myName.toUpperCase());
+
+
+// 3. to LowerCase
+
+// let myName = "KAMRUL Hasan RAFI";
+
+// console.log(myName.toLowerCase());
+
+
+
+// 4.type of variable
+
+// let myAge = "56";
+
+// console.log(typeof(myAge));
+
+
+
+// 5.adding two variable
+// let firstName = "ami";
+
+// let lastName = "rafi";
+
+// console.log(firstName+' ' +lastName);
+
+
+// 6.problem
+// let myAge = 21;
+
+// console.log(`I'm ${myAge} years old.`);
+
